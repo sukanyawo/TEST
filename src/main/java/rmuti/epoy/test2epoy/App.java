c95a9e30a3f03f6c54ffedf67a4577db6b23050d
POY <993sukunya@gmail.com>
2017-04-10 12:16:29 +0700

    UU

---
+++ src/main/java/rmuti/epoy/test2epoy/App.java
@@ -0,0 +1,11 @@
+package rmuti.epoy.test2epoy;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	NumberUtil Number = new NumberUtil();
+    		int[] arr = {2,3,4,5,6};
+        System.out.println(Number.sumArray(arr));
+    }
+}
---
+++ src/main/java/rmuti/epoy/test2epoy/NumberUtil.java
@@ -0,0 +1,12 @@
+package rmuti.epoy.test2epoy;
+
+public class NumberUtil {
+	public int sumArray(int []arr){
+		int i;
+		int sum = 0;
+		for(i=0;i<arr.length;i++){
+			sum = sum + arr[i];
+		}
+return sum;
+}
+}
